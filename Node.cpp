4c618a011346f4e3769eb780d5a980c0868dee40
Gaurav Dama <81654047+gauravsdama@users.noreply.github.com>
2024-04-13 10:05:00 -0500

    Node.cpp

--- Node.cpp
+++ Node.cpp
@@ -4,11 +4,11 @@
 using std::string;
 
 Node::Node() : next(nullptr) {
-	// TODO: implement this function
+	
 }
 
 Node::Node(string id, int year, int month, double temperature) : data(id, year, month, temperature), next(nullptr)  {
-	// TODO: implement this function
+	
 }
 
 bool Node::operator<(const Node& b) {	
