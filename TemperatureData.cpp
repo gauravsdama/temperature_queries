995032a91cff14f534e9f20606440dd477196a42
Gaurav Dama <81654047+gauravsdama@users.noreply.github.com>
2024-04-13 10:04:18 -0500

    TemperatureData.cpp

--- TemperatureData.cpp
+++ TemperatureData.cpp
@@ -2,17 +2,17 @@
 using std::string;
 
 TemperatureData::TemperatureData() : id(""), year(0), month(0), temperature(0.0) {
-	// TODO: implement this function
+	
 }
 
 TemperatureData::TemperatureData(string id, int year, int month, double temperature) : id(id), year(year), month(month), temperature(temperature) {
-	// TODO: implement this function
+	
 }
 
 TemperatureData::~TemperatureData() {}
 
 bool TemperatureData::operator<(const TemperatureData& b) {
-	// TODO: implement this function
+	
 	if (this->id != b.id) {
         return this->id < b.id;
     } else if (this->year != b.year) {
